9e787ba24250366e6a0cbc4250cf8a7a17d78cba
Sergiy Kozachenko <sergiy.kozachenko@gmail.com>
2013-06-30 15:23:27 +0300

    Minor fixes

--- app.js
+++ app.js
@@ -25,29 +25,12 @@ app.get('/', function(request, response) {
     response.render('layout.html')
 });
 
-//app.get('/:view', function(request, response) {
-//    var view = request.params.view;
-//    response.render(view)
-//});
-
 app.get('/partials/:name', function(req, res){
     var name = req.params.name;
     res.render('partials/' + name);
 });
 
-// Config Api
-
-//var mbac = require('./routes/mbac');
-//
-//app.get('/api/Practice/GetPracticesForCurrentUser', mbac.GetPracticesForCurrentUser);
-//
-//app.get('/api/Billing/AgingBillList', mbac.AgingBillList);
-//
-//// Config cron job
-//var migrateJob = require('./cron_jobs/migrate_job');
-//migrateJob.StartMigrateJob();
-
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
-  console.log("wbserg running on " + port);
+  console.log("Snippets running on " + port);
 });
